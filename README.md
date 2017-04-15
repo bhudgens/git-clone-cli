f10eab8cdfd4cd8d633e57db175643f6143c62cd
Benjamin Hudgens <benjamin@benjamindavid.com>
2017-04-15 08:37:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Overview
+
+Given a list of GitHub users or orgs, caches all the GitHub metadata and makes searching and cloning repos super fast.
+
+## What is it
+
+```bash
+hmm
+```
+
+## Help:
+
+```bash
+```
---
+++ config/config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/********************************************************************
+ * Configuration
+ ********************************************************************/
+
+/*eslint no-process-env: "off"*/
+/*eslint no-void: "off"*/
+
+module.exports = {
+  logging: typeof process.env.ENABLE_LOGGING !== "undefined",
+  debug: typeof process.env.ENABLE_DEBUG !== "undefined",
+  verbose: typeof process.env.ENABLE_VERBOSE !== "undefined",
+  username: process.env.GITHUB_USERNAME,
+  password: process.env.GITHUB_PASSWORD,
+  githubApiUrl: process.env.GITHUB_API_URL || "https://api.github.com",
+  githubOrg: process.env.GITHUB_ORGS && process.env.GITHUB_ORGS.split(','),
+  githubMaxConcurrency: process.env.GITHUB_MAXCONCURRENCY || 100,
+  githubAuthHeader: process.env.GITHUB_AUTH_HEADER,
+  repoCacheFile: process.env.REPO_CACHE_FILE || `${process.env.HOME}/Library/Caches/repocache`
+};
