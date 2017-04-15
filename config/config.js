575b82679fbbdbab721278ea252a86594fbdd527
Benjamin Hudgens <benjamin@benjamindavid.com>
2017-04-15 09:07:11 -0500

    Some small cleanup

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ module.exports = {
   username: process.env.GITHUB_USERNAME,
   password: process.env.GITHUB_PASSWORD,
   githubApiUrl: process.env.GITHUB_API_URL || "https://api.github.com",
-  githubOrg: process.env.GITHUB_ORGS && process.env.GITHUB_ORGS.split(','),
+  githubOrgs: process.env.GITHUB_ORGS,
   githubMaxConcurrency: process.env.GITHUB_MAXCONCURRENCY || 100,
   githubAuthHeader: process.env.GITHUB_AUTH_HEADER,
   repoCacheFile: process.env.REPO_CACHE_FILE || `${process.env.HOME}/Library/Caches/repocache`
